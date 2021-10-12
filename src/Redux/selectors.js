c8a6a39ab012d075058ab26711dc153f0d6d681d
Kostia Chaikovskyi <chaichka175@gmail.com>
2021-10-12 11:59:58 +0300

    add memo getviscont

--- src/Redux/selectors.js
+++ src/Redux/selectors.js
@@ -1,12 +1,24 @@
+import { createSelector } from "@reduxjs/toolkit";
+
 export const getContacts = (state) => state.items.contacts;
 
 export const getFilter = (state) => state.items.filter;
 
-export const getVisibleContacts = (state) => {
-  const contacts = getContacts(state);
-  const filter = getFilter(state);
-  const normalizedFilter = filter.toLowerCase();
-  return contacts.filter((item) =>
-    item.name.toLowerCase().includes(normalizedFilter)
-  );
-};
+// export const getVisibleContacts = (state) => {
+//   const contacts = getContacts(state);
+//   const filter = getFilter(state);
+//   const normalizedFilter = filter.toLowerCase();
+//   return contacts.filter((item) =>
+//     item.name.toLowerCase().includes(normalizedFilter)
+//   );
+// };
+
+export const getVisibleContacts = createSelector(
+  [getContacts, getFilter],
+  (contacts, filter) => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter((item) =>
+      item.name.toLowerCase().includes(normalizedFilter)
+    );
+  }
+);
